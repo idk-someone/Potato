dce897946aad437fd03f05d2226ea5ccc62f78b2
idk-someone <teodoradelina20@gmail.com>
2025-06-04 22:11:38 +0300

    first commit
    first

---
+++ script.js
@@ -0,0 +1,38 @@
+function openHome()
+{
+
+    window.location.href = "index.html";
+}
+
+function openTypes()
+{
+
+    window.location.href = "types.html";
+}
+
+function openDishes()
+{
+
+    window.location.href = "dishes.html";
+}
+
+function openHistory()
+{
+
+    window.location.href = "history.html";
+}
+
+function openPotatoCare()
+{
+
+    window.location.href = "potatocare.html";
+}
+
+function openVodka()
+{
+
+    window.location.href = "vodka.html";
+}
+
+
+
